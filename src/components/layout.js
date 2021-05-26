a99a845436b98cd5b97d951fad9719feed57256a
Yasuda <mittya6@gmail.com>
2021-05-26 22:20:00 +0900

    init

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,8 +6,6 @@ export default ({ children }) => (
     <Helmet>
       <title>Gastbyサンプル</title>
       <meta name="description" content="このページはGastbyサンプルです。" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/toolbar/prism-toolbar.min.js" integrity="sha512-cu2C9EssrOrVXT4thyL4gz/qWyh3Lq9XbICUXYyh3zJRCSKk1J08tBKPXnsSpdpZXOliaK/OJBygw/l0twAmwA==" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js" integrity="sha512-bWzyGaP/f19RLeYGN6ZhDgvkS7GM0Fq23lOI1/PB3lV6I775RIDzXLxCGR4iiDGzeMsQ3lncuXUQMFP7qO9lIQ==" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.20/js/uikit.min.js" integrity="sha512-SKcdAvrPgF8Iy45+CmcEwF4S2uhVLyTNM8o4UVfggZCQbUIv69qT+b2ppvFlud86827BuRNe7CtCrppmkmdOvQ==" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.20/js/uikit-icons.min.js" integrity="sha512-5kjeg68TagkjC5zqP4kfrzqewo2G7mku+uYr0UImSe/FsjIDdh6JGN1XvxRznmXJe2ZZ+epKPfHE8amCM9bZ8Q==" crossorigin="anonymous"></script>
       <html lang="ja" />
